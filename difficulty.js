1462c6781f1e985ae3956ab914cd1ef637952f46
BAD <soccerskin87@gmail.com>
2022-12-03 10:39:06 +0000

    GUI + difficultty in the works +start pos

---
+++ difficulty.js
@@ -0,0 +1,10 @@
+function GetDif(first, second){
+    const hello = document.getElementById("lol").value
+    hello+1
+    first-1
+    second+1
+    if(hello < second+1 && hello > first-1){
+        return parseInt(hello)
+    }
+    return 15
+}
--- sketch.js
+++ sketch.js
@@ -4,8 +4,12 @@ let speed_player = 5;
 let canvas = 400;
 let big = 55
 let big_square_enemy = 25
-
-
+let playing = false;
+let myFont;
+let pleaseonlydothisonce = false;
+function preload() {
+    myFont = loadFont('./assets/Inconsolata-SemiBold.ttf');
+  }
 function setup() {
     createCanvas(canvas, canvas);
 }
@@ -19,33 +23,49 @@ const is_key_down = (() => {
 })();
   
 function draw() {
-    background(155);
-    fill("#FF0000");
-    circle(30, 30, 20);
-    
-    
-    background(155);
-    
-   
-    for (let i = 0; i < currentsquare.length; i++) {
-        currentsquare[i].crsquare()
-        currentsquare[i].changey()
-        currentsquare[i].check_if_off_screen()
+    if(playing == true){
+        console.log(y_player)
         
-        fill("#ba3d34")
-        currentsquare[i].checkifhittingplayer()
+        background(155);
+        if(pleaseonlydothisonce == false){
+            currentsquare = []
+            x_player = canvas/2-(big/2);
+            y_player = canvas - big / 2;
+            pleaseonlydothisonce = true
+        }
+       
+        for (let i = 0; i < currentsquare.length; i++) {
+            currentsquare[i].crsquare()
+            currentsquare[i].changey()
+            currentsquare[i].check_if_off_screen()
+            
+            fill("#ba3d34")
+            currentsquare[i].checkifhittingplayer()
+            
+        }
+        fill(255)
+        square(x_player, y_player, big);
+        playermcheck(true)
+    }else if(playing == false){
+        background(155)
+        textSize(26.5);
+        textFont(myFont);
+        text('Click "Enter" to start playing', 0, canvas / 2);
+        pleaseonlydothisonce = false
         
     }
-    fill(255)
-    square(x_player, y_player, big);
-    playermcheck()
+   
     
 }
+function keyPressed() {
+    if(playing == false){
+        playing = true
+    }
+  }
 
 
-
-
-function playermcheck(){
+let pushs = 0;
+function playermcheck(cs){
     if (is_key_down("w") == true) {
         y_player -= speed_player;
     } else if (is_key_down("s") == true) {
@@ -58,25 +78,31 @@ function playermcheck(){
     }
 
     if(x_player > canvas - big){
-        x_player -= speed_player
+        x_player -= speed_player + pushs
     }else if(x_player == 0 || 0 > x_player){
         x_player = 1  
     }
     if(y_player > canvas - big){
-        y_player -= speed_player
+        y_player -= speed_player + pushs
     }else if(y_player == 0 || 0 > y_player){
         y_player  = 1
+    }
+    if(cs == true && y_player > 342.5){
+        pushs = 1.5
+        console.log("speedy boy")
+    }else{
+        pushs = 0
     }
 }
 
 
 let currentsquare = []
-const sqr_on_screen = 50
-const size = 50;
+//const sqr_on_screen = 50
+//const size = 50;
 
 
 
-var intervalID = window.setInterval(test, 100);
+var intervalID = window.setInterval(test, 100-15 + GetDif(15, 30));
 //function mouseClicked() {
 //    currentsquare.push(new Square(0-big));
 //}
@@ -110,6 +136,7 @@ class Square {
         y_player > this.y - big_square_enemy / 2 &&
         y_player < this.y + big_square_enemy / 2){
             console.log("bruh lol")
+            playing = false
         }
     }
     
